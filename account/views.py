290053f8ff6e1e290605e0325e63e663bb326ba7
lemar30 <psilozip@mail.ru>
2020-02-05 21:56:09 +0600

    еще удалил

--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.contrib import messag
+from django.contrib import mes
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import (
