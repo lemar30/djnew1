7dab221054e9e687da812aeadff1f933771b692f
lemar30 <psilozip@mail.ru>
2020-02-08 21:51:52 +0600

    rfg

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from allauth.account.views import login
 from . import views
 from account import views as user_views
 
+
 urlpatterns = [
     path('signup/', user_views.SignupView.as_view(template_name="account/signup.html"), name='signup'),
     path('login/', user_views.SignupView.as_view(template_name="account/login.html"), name='login'),
